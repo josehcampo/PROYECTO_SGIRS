b0a2459dd87d85e54bbfa8c17837e959d36a0ba3
jose <josecampo1972@gmail.com>
2023-03-26 23:45:31 -0500

    Crear-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <a href="/">SGIRS</a>
+      </header>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
